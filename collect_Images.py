7211f82de66b51de735ad601d161222c2456a82d
Syber <smagwaza023@student.wethinkcode.co.za>
2024-06-03 16:16:04 +0200

    fully done with the frame

--- collect_Images.py
+++ collect_Images.py
@@ -3,6 +3,12 @@ import os
 
 
 cam = cv2.VideoCapture(0)
+instructions = "Press Q to quit and C to capture images."
+font = cv2.FONT_ITALIC
+position = (5, 30)  # Position for the text
+font_scale = 0.9
+color = (0, 0, 0)  
+thickness = 1
 
 image_classes = 3
 
@@ -10,6 +16,8 @@ for i in range(image_classes):
     while True:
         ret, frame = cam.read()
         flip = cv2.flip(frame,1)
+
+        cv2.putText(flip, instructions, position, font, font_scale, color, thickness)
         cv2.imshow("webcam", flip)
         key = cv2.waitKey(1) & 0xFF
         
