28103be2ba9602607408ca2d3192bcf51048b042
Syber <smagwaza023@student.wethinkcode.co.za>
2024-06-03 15:45:19 +0200

    fixed the code and can now save 100 images in specific classes for future use

--- collect_Images.py
+++ collect_Images.py
@@ -1,28 +1,31 @@
 import cv2
 import os
 
-Data_Dir = "Desktop/HandSigns/data"
-base_dir = os.path.expanduser("~")
-full_path = os.path.join(base_dir, Data_Dir)
-if not os.path.exists(full_path):
-    os.makedirs(full_path)
 
 cam = cv2.VideoCapture(0)
 
-image_count = 0
+image_classes = 3
 
-while True:
-    ret, frame = cam.read()
-    cv2.imshow("webcam", frame)
-    key = cv2.waitKey(1) & 0xFF
-    
-    if key == ord("q"):
-        break
-    if key == ord("c"):
-        image_name = "captured image{}.jpg".format(image_count)
-        image_save = os.path.join(full_path, image_name)
-        cv2.imwrite(image_save, frame)
-        image_count += 1
+for i in range(image_classes):
+    while True:
+        ret, frame = cam.read()
+        flip = cv2.flip(frame,1)
+        cv2.imshow("webcam", flip)
+        key = cv2.waitKey(1) & 0xFF
+        
+        if key == ord("q"):
+            break
+        if key == ord("c"):
+            for j in range (100):
+                image_name = f"captured image {j}.jpg"
+                Data_Dir = f"Desktop/HandSigns/data/{i}"
+                base_dir = os.path.expanduser("~")
+                full_path = os.path.join(base_dir, Data_Dir)
+                if not os.path.exists(full_path):
+                    os.makedirs(full_path)
+                image_save = os.path.join(full_path, image_name)
+                cv2.imwrite(image_save, frame)
+            break
         
 cam.release()  
 cv2.destroyAllWindows()
