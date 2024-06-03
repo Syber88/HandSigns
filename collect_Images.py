bb4bc9b05e7a946818d5cce92cc89c23aecba1ba
Syber <smagwaza023@student.wethinkcode.co.za>
2024-06-03 10:29:33 +0200

    recommit

--- collect_Images.py
+++ collect_Images.py
@@ -1,6 +1,12 @@
 import cv2
 import os
 
+Data_Dir = "Desktop/HandSigns/data"
+base_dir = os.path.expanduser("~")
+full_path = os.path.join(base_dir, Data_Dir)
+if not os.path.exists(full_path):
+    os.makedirs(full_path)
+
 cam = cv2.VideoCapture(0)
 
 image_count = 0
@@ -13,9 +19,10 @@ while True:
     if key == ord("q"):
         break
     if key == ord("c"):
-        pass
+        image_name = "captured image{}.jpg".format(image_count)
+        image_save = os.path.join(full_path, image_name)
+        cv2.imwrite(image_save, frame)
+        image_count += 1
         
-    
-    
 cam.release()  
 cv2.destroyAllWindows()
