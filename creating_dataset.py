42728c386183f234d7ad8bcf0a3cdf0affce798d
Syber <smagwaza023@student.wethinkcode.co.za>
2024-06-07 15:02:17 +0200

    config hands

--- creating_dataset.py
+++ creating_dataset.py
@@ -1,3 +1,10 @@
 import cv2
 import os 
-import  as mp
+import  mediapipe as mp
+
+
+mp_hands = mp.solutions.hands 
+mp_drawings = mp.solutions.drawing_utils
+mp_drawingUtils = mp.solutions.drawing_styles
+hands = mp_hands.Hands(static_image_mode = True, min_detection_confidence = 0.4)
+
