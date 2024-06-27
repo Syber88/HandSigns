3caac7df907495d75088183d9eb8c738c5392f47
Siyabonga Magwaza <smagwaza023@student.wethinkcode.co.za>
2024-06-27 11:17:23 +0200

    created the model and trained it

--- train.py
+++ train.py
@@ -10,4 +10,13 @@ data_file = pickle.load(open("./data.pickle", "rb"))
 data = np.asarray(data_file["data"])
 labels = np.asarray(data_file["labels"])
 
-x_train, x_test, y_train, y_test = train_test_split(data, labels, stratify=labels, shuffle=True,test_size=0.2)
+x_train, x_test, y_train, y_test = train_test_split(data, labels, stratify=labels, shuffle=True, test_size=0.2)
+
+model = RandomForestClassifier()
+model.fit(x_train, y_train)
+
+y_predict = model.predict(x_test)
+
+
+
+
