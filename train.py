0e0f264b9ed99dcddd81cf661f6ae624ff08ec32
Siyabonga Magwaza <smagwaza023@student.wethinkcode.co.za>
2024-06-27 11:16:32 +0200

    split the data into training and testing

--- train.py
+++ train.py
@@ -9,3 +9,5 @@ data_file = pickle.load(open("./data.pickle", "rb"))
 
 data = np.asarray(data_file["data"])
 labels = np.asarray(data_file["labels"])
+
+x_train, x_test, y_train, y_test = train_test_split(data, labels, stratify=labels, shuffle=True,test_size=0.2)
