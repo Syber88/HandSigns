9e18a143ecb4a0a8deda6493c293ccc740377a1a
Siyabonga Magwaza <smagwaza023@student.wethinkcode.co.za>
2024-06-27 18:26:24 +0200

    there should be landmarks drawn on the webcam output but currently having issues with my screen. will confirm if issue is isolated or not

--- tainclassifier.py
+++ tainclassifier.py
@@ -12,15 +12,22 @@ hands = mp_hands.Hands(static_image_mode = True, min_detection_confidence = 0.3)
 while True:
     
     ret, frame = video.read()
-    flip = cv2.flip(frame,1)
-    rgb_flip = cv2.cvtColor(flip, cv2.COLOR_BGR2RGB)
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    frame_flip = cv2.flip(rgb_frame,1)
     
-    processed = hands.process(rgb_flip)
+    processed = hands.process( rgb_frame)
         
     if processed.multi_hand_landmarks:
+        for landmarks in processed.multi_hand_landmarks:
+            mp_drawings.draw_landmarks(
+                frame,  landmarks, 
+                mp_hands.HAND_CONNECTIONS,  
+                mp_drawingUtils.get_default_hand_landmarks_style(),
+                mp_drawingUtils.get_default_hand_connections_style()
+            )
     
-    cv2.imshow("frame", rgb_flip)
+    cv2.imshow("frame",  frame)
     cv2.waitKey(25)
     
-video.release()
-cv2.destroyAllWindows()
+    video.release()
+    cv2.destroyAllWindows()
