2468c8cd9eff0b516cc1046fd11de16c046e3311
Syber <smagwaza023@student.wethinkcode.co.za>
2024-06-28 16:07:59 +0200

    made the box a little nicer

--- inference_classifier.py
+++ inference_classifier.py
@@ -51,26 +51,26 @@ while True:
                 data_aux.append(x - min(x_))
                 data_aux.append(y - min(y_))
                 
-                x1 = int(min(x_) * W)
-                y1 = int(min(y_) * H)
+                x1 = int(min(x_) * W) - 10
+                y1 = int(min(y_) * H) - 10
                 
-                x2 = int(max(x_) * W)
-                y2 = int(max(y_) * H)
+                x2 = int(max(x_) * W) - 10
+                y2 = int(max(y_) * H) - 10
             
         prediction = model.predict([np.asarray(data_aux)])
         predicted_letter = labels_dict[int(prediction[0])]
         print(predicted_letter)
         
         
-    bgr_frame = cv2.cvtColor(frame_flip, cv2.COLOR_RGB2BGR)
-    cv2.rectangle(bgr_frame, (x1, y1), (x2, y2), (0,0,0), 4)
-    cv2.putText(bgr_frame, predicted_letter, (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 0, 0), 3, 
-                cv2.LINE_AA)
+        bgr_frame = cv2.cvtColor(frame_flip, cv2.COLOR_RGB2BGR)
+        cv2.rectangle(bgr_frame, (x1, y1), (x2, y2), (0,0,0), 4)
+        cv2.putText(bgr_frame, predicted_letter, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 0, 0), 3, 
+                    cv2.LINE_AA)
             
                     
     cv2.imshow("frame", bgr_frame)
     if cv2.waitKey(1) == ord('q'):
         break
-    
+     - 10
 video.release()
 cv2.destroyAllWindows()
