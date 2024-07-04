65dd784b6041994171e627dbfd3b13f9b855042b
Siyabonga Magwaza <148061326+Syber88@users.noreply.github.com>
2024-07-04 16:09:05 +0200

    Update train.py

--- train.py
+++ train.py
@@ -4,22 +4,28 @@ from sklearn.metrics import accuracy_score
 import pickle
 import numpy as np
 
+# Load the data and labels from the pickle file
 data_file = pickle.load(open("./data.pickle", "rb"))
 
-data = np.asarray(data_file["data"])
-labels = np.asarray(data_file["labels"])
+data = np.asarray(data_file["data"])  # Convert data to a NumPy array
+labels = np.asarray(data_file["labels"])  # Convert labels to a NumPy array
 
+# Split the data into training and testing sets
 x_train, x_test, y_train, y_test = train_test_split(data, labels, stratify=labels, shuffle=True, test_size=0.2)
 
+# Initialize and train the RandomForestClassifier
 model = RandomForestClassifier()
 model.fit(x_train, y_train)
 
+# Predict the labels for the test set
 y_predict = model.predict(x_test)
 
+# Calculate the accuracy of the model
 score = accuracy_score(y_predict, y_test)
 
-print(f"{score*100}% classified correctly")
+# Print the accuracy score
+print(f"{score * 100}% classified correctly")
 
-f = open("model.pickle", "wb")
-pickle.dump({"model":model},f)
-f.close()
+# Save the trained model to a pickle file
+with open("model.pickle", "wb") as f:
+    pickle.dump({"model": model}, f)
