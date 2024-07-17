fc23565646638acf7bece75f57821fabbdd817c5
Syber <smagwaza023@student.wethinkcode.co.za>
2024-07-17 14:22:38 +0200

    reusable function to apply on images and get results based off the model used

--- Handsigns2.0.py
+++ Handsigns2.0.py
@@ -10,6 +10,14 @@ mp_holistic = mp.solutions.mediapipe.solutions.holistic #holistic model
 # mp_drawing = mp.solutions.drawing_utils #drawign utilities
 mp_drawing = mp.solutions.mediapipe.solutions.drawing_utils
 
+def mediapipe_detection(image, model):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image.flags.writeable = False
+    results = model.process(image)
+    image.flags.writeable = True
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    return image, results 
+
 def draw_landmarks(image, results):
     mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_TESSELATION,
                               mp_drawing.DrawingSpec(color=(80,110,10), thickness=1, circle_radius=1),
