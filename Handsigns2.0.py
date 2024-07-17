a3158e8eb8f4c76b1bb446de3b3439e27ff97818
Syber <smagwaza023@student.wethinkcode.co.za>
2024-07-17 14:19:42 +0200

    we will not be using mediapipe holistic

--- Handsigns2.0.py
+++ Handsigns2.0.py
@@ -4,3 +4,8 @@ import matplotlib.pyplot as plt
 import time 
 import mediapipe as mp 
 import os 
+
+# mp.solutions.mediapipe.
+mp_holistic = mp.solutions.mediapipe.solutions.holistic #holistic model
+# mp_drawing = mp.solutions.drawing_utils #drawign utilities
+mp_drawing = mp.solutions.mediapipe.solutions.drawing_utils
